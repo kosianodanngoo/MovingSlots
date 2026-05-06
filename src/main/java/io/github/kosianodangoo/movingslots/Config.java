010fb5afd999ca8075dc3795ba5d321b3218045e
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-05-06 20:00:38 +0900

    ボタンなども動く

--- src/main/java/io/github/kosianodangoo/movingslots/Config.java
+++ src/main/java/io/github/kosianodangoo/movingslots/Config.java
@@ -12,15 +12,24 @@ public class Config {
     public static final ForgeConfigSpec.DoubleValue MIN_INITIAL_VELOCITY = BUILDER.comment("Min Initial Velocity").defineInRange("minInitialVelocity", 40, 0, Double.POSITIVE_INFINITY);
     public static final ForgeConfigSpec.DoubleValue MAX_INITIAL_VELOCITY = BUILDER.comment("Max Initial Velocity").defineInRange("maxInitialVelocity", 160, 0, Double.POSITIVE_INFINITY);
 
+    public static final ForgeConfigSpec.BooleanValue MOVE_WIDGETS = BUILDER.comment("Whether to Move Widgets").define("moveWidgets", true);
+    public static final ForgeConfigSpec.BooleanValue MOVE_SLOTS = BUILDER.comment("Whether to Move Slots").define("moveSlots", true);
+
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
     private static double minInitialVelocity;
     private static double randomDelta;
 
+    private static boolean moveWidgets;
+    private static boolean moveSlots;
+
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
         minInitialVelocity = MIN_INITIAL_VELOCITY.get();
         randomDelta = MAX_INITIAL_VELOCITY.get() - minInitialVelocity;
+
+        moveWidgets = MOVE_WIDGETS.get();
+        moveSlots = MOVE_SLOTS.get();
     }
 
     public static double getMinInitialVelocity() {
@@ -29,5 +38,13 @@ public class Config {
 
     public static double getRandomDelta() {
         return randomDelta;
+    }
+
+    public static boolean shouldMoveWidgets() {
+        return moveWidgets;
+    }
+
+    public static boolean shouldMoveSlots() {
+        return moveSlots;
     }
 }
---
+++ src/main/java/io/github/kosianodangoo/movingslots/WidgetMotion.java
@@ -0,0 +1,5 @@
+package io.github.kosianodangoo.movingslots;
+
+public interface WidgetMotion {
+    void movingSlots$tick(int screenWidth, int screenHeight);
+}
